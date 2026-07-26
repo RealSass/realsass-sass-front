397c0907858d82c7d731a042cf6dbb88c6a37f66
agustin <agustinjalilleon@gmail.com>
2026-07-26 01:11:14 -0300

    qweqwqewqeweqweqw

--- lib/trpc/router-type.ts
+++ lib/trpc/router-type.ts
@@ -1,4 +1,11 @@
 /**
  * lib/trpc/router-type.ts
+ *
+ * Re-exporta el AppRouter como tipo opaco para evitar
+ * que TypeScript compile el source del back.
+ * Usamos import type para que sea solo a nivel de tipos.
  */
-export type { AppRouter } from '../../../realsass-sass-back/src/trpc/app-router';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import type { AppRouter } from '../../../realsass-sass-back/src/trpc/app-router';
+
+export type { AppRouter };
